4afd5dc274bf7c8a8876f83684e11fdd2091db0a
Jschammy <schamel.an.justin@gmail.com>
2025-04-25 09:57:07 -0400

    Added comments and notes in main.py to improve readability.

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
+# Import modules for game.
 from turtle import Screen
 import time
 from snake import Snake
 from food import Food
 from scoreboard import ScoreBoard
 
+# Instantiate classes for screen.
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Justin's Snake Game")
 screen.tracer(0)
 
+# Instantiate classes for snake, food, and the scoreboard.
 snake = Snake()
 food = Food()
 scoreboard = ScoreBoard()
 
+# Listen to user input.
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
+# Main game loop.
 game_on = True
 while game_on:
     screen.update()
